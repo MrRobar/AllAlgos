1c1cbece56f73eeaf295b72c0af4cceec40c575b
MrRobar <mr.robariii@gmail.com>
2023-06-26 15:38:32 +0200

    Initial
    All Unit tests are done

---
+++ AllAlgosCombined/MergeSortBenchmark.cs
@@ -0,0 +1,28 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Order;
+using NUnit.Framework;
+
+namespace AllAlgosCombined
+{
+    [MemoryDiagnoser]
+    [Orderer(SummaryOrderPolicy.FastestToSlowest)]
+    [RankColumn]
+    public class MergeSortBenchmark
+    {
+
+        private int[] valuesToCheck = new int[10_000];
+        private static readonly MergeSortVariant mergeSortVariant = new MergeSortVariant();
+
+        [SetUp]
+        public void Setup()
+        {
+            mergeSortVariant.FillArrayRandomly(valuesToCheck);
+        }
+
+        [Benchmark]
+        public void RunMergeSort()
+        {
+            mergeSortVariant.MergeSort(valuesToCheck, 0, valuesToCheck.Length - 1);
+        }
+    }
+}
---
+++ AllAlgosCombined/Program.cs
@@ -0,0 +1,13 @@
+﻿using BenchmarkDotNet.Running;
+
+namespace AllAlgosCombined
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            BenchmarkRunner.Run<MergeSortBenchmark>();
+        }
+    }
+}
